b9a2cf519d7f1cec3f3209fd3c39f1aaf3f8518c
Maksym Rohachov <romax982@gmail.com>
2020-07-07 17:40:25 +0300

    Added UserDetailsService implementation which used in Security configuration

--- src/main/java/com/softserve/springmvc/security/SecurityWebApplicationInitializer.java
+++ src/main/java/com/softserve/springmvc/security/SecurityWebApplicationInitializer.java
@@ -1,8 +1,12 @@
 package com.softserve.springmvc.security;
 
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
 /**
  * @author invzbl3 on 07.07.2020.
  * @project SpringApplication
  */
-public class SecurityWebApplicationInitializer {
+
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+
 }
