91295d44f67b88559fce83c7a415c88a50aa0170
Maksym Rohachov <romax982@gmail.com>
2020-07-07 20:51:55 +0300

    Created Services

--- src/main/java/com/softserve/springmvc/service/UserProfileService.java
+++ src/main/java/com/softserve/springmvc/service/UserProfileService.java
@@ -1,8 +1,18 @@
 package com.softserve.springmvc.service;
 
+import java.util.List;
+import com.softserve.springmvc.model.UserProfile;
+
 /**
  * @author invzbl3 on 07.07.2020.
  * @project SpringApplication
  */
-public class UserProfileService {
+
+public interface UserProfileService {
+
+    UserProfile findById(int id);
+
+    UserProfile findByType(String type);
+
+    List<UserProfile> findAll();
 }
--- src/main/java/com/softserve/springmvc/service/UserService.java
+++ src/main/java/com/softserve/springmvc/service/UserService.java
@@ -1,8 +1,26 @@
 package com.softserve.springmvc.service;
 
+import java.util.List;
+import com.softserve.springmvc.model.User;
+
 /**
  * @author invzbl3 on 07.07.2020.
  * @project SpringApplication
  */
-public class UserService {
+
+public interface UserService {
+
+    User findById(int id);
+
+    User findBySSO(String sso);
+
+    void saveUser(User user);
+
+    void updateUser(User user);
+
+    void deleteUserBySSO(String sso);
+
+    List<User> findAllUsers();
+
+    boolean isUserSSOUnique(Integer id, String sso);
 }
