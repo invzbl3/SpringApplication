1e59caf5bc791a2cd06ed90835ea69b16ee31cae
Maksym Rohachov <romax982@gmail.com>
2020-07-07 18:15:44 +0300

    Added logic for RoleToUserProfileConverter

--- src/main/java/com/softserve/springmvc/converter/RoleToUserProfileConverter.java
+++ src/main/java/com/softserve/springmvc/converter/RoleToUserProfileConverter.java
@@ -1,8 +1,37 @@
 package com.softserve.springmvc.converter;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+
+import com.softserve.springmvc.model.UserProfile;
+import com.softserve.springmvc.service.UserProfileService;
+
 /**
  * @author invzbl3 on 07.07.2020.
  * @project SpringApplication
  */
-public class RoleToUserProfileConverter {
+
+/**
+ * A converter class used in views to map id's to actual userProfile objects.
+ */
+@Component
+public class RoleToUserProfileConverter implements Converter<Object, UserProfile>{
+
+    static final Logger logger = LoggerFactory.getLogger(RoleToUserProfileConverter.class);
+
+    @Autowired
+    UserProfileService userProfileService;
+
+    /**
+     * Gets UserProfile by Id
+     */
+    public UserProfile convert(Object element) {
+        Integer id = Integer.parseInt((String)element);
+        UserProfile profile= userProfileService.findById(id);
+        logger.info("Profile : {}",profile);
+        return profile;
+    }
 }
