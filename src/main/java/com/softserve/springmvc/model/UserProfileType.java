480e11f5bdeccc86fec420b868839b4da40c8592
Maksym Rohachov <romax982@gmail.com>
2020-07-07 20:12:54 +0300

    Created Models

--- src/main/java/com/softserve/springmvc/model/UserProfileType.java
+++ src/main/java/com/softserve/springmvc/model/UserProfileType.java
@@ -1,8 +1,24 @@
 package com.softserve.springmvc.model;
 
+import java.io.Serializable;
+
 /**
  * @author invzbl3 on 07.07.2020.
  * @project SpringApplication
  */
-public class UserProfileType {
+
+public enum UserProfileType implements Serializable{
+    USER("USER"),
+    DBA("DBA"),
+    ADMIN("ADMIN");
+
+    String userProfileType;
+
+    private UserProfileType(String userProfileType){
+        this.userProfileType = userProfileType;
+    }
+
+    public String getUserProfileType(){
+        return userProfileType;
+    }
 }
