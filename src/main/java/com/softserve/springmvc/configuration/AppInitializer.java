5a916ea75685d32765306f9a41702c4a842a29e8
Maksym Rohachov <romax982@gmail.com>
2020-07-07 18:42:48 +0300

    Updated the Spring Initializer class

--- src/main/java/com/softserve/springmvc/configuration/AppInitializer.java
+++ src/main/java/com/softserve/springmvc/configuration/AppInitializer.java
@@ -1,8 +1,26 @@
 package com.softserve.springmvc.configuration;
 
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
 /**
  * @author invzbl3 on 07.07.2020.
  * @project SpringApplication
  */
-public class AppInitializer {
+
+public class AppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[] { AppConfig.class };
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] { "/" };
+    }
 }
